ec3107cacb26d4815a124883836be101b8619a70
mualifin22 <mimualifin22@gmail.com>
2024-07-30 20:59:56 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +17,65 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        User::create([
+            'name' => 'Inuma',
+            'email' => 'admin@example.com',
+            'password' => 'bcrypt(12345)',
+        ]);
+
+        User::create([
+            'name' => 'Mualifin',
+            'email' => 'mualifin@example.com',
+            'password' => 'bcrypt(12345)',
+        ]);
+
+        Category::create([
+            'name' => 'Technology',
+            'slug' => 'technology',
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Judul pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, quod?',
+            'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quibusdam, sed ad aut corrupti porro suscipit eligendi assumenda ex, nesciunt veniam ipsum. Tenetur, repellat qui optio incidunt autem ex magnam corporis praesentium? Ex quo, omnis exercitationem id qui eligendi tempora quis impedit. Necessitatibus, aperiam sapiente. Animi voluptatibus temporibus dolore fugit obcaecati id ab sint, ipsum distinctio totam, enim, facilis deserunt. Veniam iure similique tempora qui molestiae quidem exercitationem cupiditate? Error quod officia aspernatur, reprehenderit, tempora assumenda, corrupti quas eius harum non aliquid unde quisquam? Consequuntur reprehenderit temporibus aspernatur? Accusamus, aliquam? Odit blanditiis beatae deserunt eveniet earum eum id laudantium repellendus, inventore sint dicta dolorem ullam? Earum ut nihil rem pariatur!',
+            'user_id' => 1,
+            'category_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, quod?',
+            'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quibusdam, sed ad aut corrupti porro suscipit eligendi assumenda ex, nesciunt veniam ipsum. Tenetur, repellat qui optio incidunt autem ex magnam corporis praesentium? Ex quo, omnis exercitationem id qui eligendi tempora quis impedit. Necessitatibus, aperiam sapiente. Animi voluptatibus temporibus dolore fugit obcaecati id ab sint, ipsum distinctio totam, enim, facilis deserunt. Veniam iure similique tempora qui molestiae quidem exercitationem cupiditate? Error quod officia aspernatur, reprehenderit, tempora assumenda, corrupti quas eius harum non aliquid unde quisquam? Consequuntur reprehenderit temporibus aspernatur? Accusamus, aliquam? Odit blanditiis beatae deserunt eveniet earum eum id laudantium repellendus, inventore sint dicta dolorem ullam? Earum ut nihil rem pariatur!',
+            'user_id' => 1,
+            'category_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, quod?',
+            'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quibusdam, sed ad aut corrupti porro suscipit eligendi assumenda ex, nesciunt veniam ipsum. Tenetur, repellat qui optio incidunt autem ex magnam corporis praesentium? Ex quo, omnis exercitationem id qui eligendi tempora quis impedit. Necessitatibus, aperiam sapiente. Animi voluptatibus temporibus dolore fugit obcaecati id ab sint, ipsum distinctio totam, enim, facilis deserunt. Veniam iure similique tempora qui molestiae quidem exercitationem cupiditate? Error quod officia aspernatur, reprehenderit, tempora assumenda, corrupti quas eius harum non aliquid unde quisquam? Consequuntur reprehenderit temporibus aspernatur? Accusamus, aliquam? Odit blanditiis beatae deserunt eveniet earum eum id laudantium repellendus, inventore sint dicta dolorem ullam? Earum ut nihil rem pariatur!',
+            'user_id' => 2,
+            'category_id' => 2,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Empat',
+            'slug' => 'judul-empat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, quod?',
+            'content' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quibusdam, sed ad aut corrupti porro suscipit eligendi assumenda ex, nesciunt veniam ipsum. Tenetur, repellat qui optio incidunt autem ex magnam corporis praesentium? Ex quo, omnis exercitationem id qui eligendi tempora quis impedit. Necessitatibus, aperiam sapiente. Animi voluptatibus temporibus dolore fugit obcaecati id ab sint, ipsum distinctio totam, enim, facilis deserunt. Veniam iure similique tempora qui molestiae quidem exercitationem cupiditate? Error quod officia aspernatur, reprehenderit, tempora assumenda, corrupti quas eius harum non aliquid unde quisquam? Consequuntur reprehenderit temporibus aspernatur? Accusamus, aliquam? Odit blanditiis beatae deserunt eveniet earum eum id laudantium repellendus, inventore sint dicta dolorem ullam? Earum ut nihil rem pariatur!',
+            'user_id' => 1,
+            'category_id' => 2,
         ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,10 +3,10 @@
 @section('container')
 <h1 class="mb=5">{{ $post->title }}</h1>
 
-<p>By. Inuma in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
 {!! $post->content !!}
 
-<a href="/posts">Back To Blog</a>
+<a href="/posts" class="text-decoration-none d-block mt-3">Back To Blog</a>
 
 @endsection
