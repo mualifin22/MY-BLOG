1fec1f37c294f7a969801a4a8d097b0f50b3fb19
mualifin22 <mimualifin22@gmail.com>
2024-08-08 21:23:15 +0700

    improve view page

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,15 @@ Route::get('/categories', function () {
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
         'posts' => $category->posts,
-        'category' => $category->name
     ]);
 });
 
 route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'title' => "Author's Posts",
+        'title' => "Post By Author : $author->name",
         'posts' => $author->posts,
         'author' => $author->name
     ]);
