1e6465c22ef0d30953bb1e9ce4c596fe64f590b7
mualifin22 <mimualifin22@gmail.com>
2024-08-09 20:01:06 +0700

    make card tittle for first post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,18 +3,24 @@
 @section('container')
     <h1 class="mb-5">{{ $title }}</h1>
 
-    @if ($post->count())
-    <div class="card mb-3">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
-                This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    @if ($posts->count())
+        <div class="card mb-3">
+            <img src="https://source.unsplash.com/daily" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h3 class="card-title">{{ $posts[0]->title }}</h3>
+                <p> <small class="text-muted">By. <a href="/authors/{{ $posts[0]->author->username }}"
+                            class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
+                            href="/categories/{{ $posts[0]->category->slug }}"
+                            class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                {{ $posts[0]->created_at->diffForHumans() }}</small></p>
+
+                <p class="card-text">{{ $posts[0]->excerpt }}</p>
+
+                <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more...</a>
+            </div>
         </div>
-    </div>
     @else
-    <p class="text-center fs-4">No post found.</p>
+        <p class="text-center fs-4">No post found.</p>
     @endif
 
     @foreach ($posts as $post)
