0b359ef8c94348e47fe4b7eb925e77eb39d0d515
mualifin22 <mimualifin22@gmail.com>
2024-08-18 16:55:30 +0700

    user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -12,5 +13,19 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' =>'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:255'
+        ]);
+
+        User::create($validatedData);
+
+        return redirect('/login')->with('success', 'Registration successfull! Please login');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,10 +3,17 @@
 @section('container')
     <div class="row justify-content-center">
         <div class="col-lg-4">
+
+            @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+
             <main class="form-signin">
                 <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
                 <form>
-
                     <div class="form-floating">
                         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                         <label for="floatingInput">Email address</label>
@@ -15,7 +22,6 @@
                         <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                         <label for="floatingPassword">Password</label>
                     </div>
-
                     <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
                 </form>
                 <small class="d-block text-center mt-3">
